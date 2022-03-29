f9773177d5294dced67239cf0ee4b37ada20cdfd
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-03-28 20:24:58 -0600

    set up server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
-const express = require('express')
+//mongodb command installs MongoDB database driver that allows -
+//Node.js application to connect to the database and work with data.
+
+//express installs the web framework for Node.js application
+
+//cors installs a Node.js package that allows cross origin resource sharing.
+
+//dotenv installs the module that loads environment variable 
+//from a .env file into process.env file
+//This lets you separate configuration files from the code.
+
+import express from "express";
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
 const app = express()
-const cors = require('cors')
-require('dotenv').config({path: './config.env'})
-const port = process.env.PORT || 5000;
+
 app.use(cors())
 app.use(express.json())
-app.use(require('./routes/record'))
-// get driver connection
-const dbo= require('./db/conn')
 
-app.listen(port, ()=>{
-    //perform a database connection when server starts
-    dbo.connectToServer(function(err){
-        if(err) console.error(err)
-    })
-    console.log(`Server is running on port: ${port}`)
-})
+app.use("/api/v1/restaurants", restaurants)
+app.use("*", (req, res=>res.statusCode(404).json({error: "not found"})))
+
+export default app
